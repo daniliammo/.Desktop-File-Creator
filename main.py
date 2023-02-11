ae3e6bfa17483665666e8d02966f2ffd30c45112
daniliammo <67186086+daniliammo@users.noreply.github.com>
2023-02-11 18:02:27 +0500

    Remove Config

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.config import Config
 from kivy.core.window import Window
+import os
 Config.set('graphics', 'width', '950')
 Config.set('graphics', 'height', '950')
 Window.clearcolor = (0.205, 0.205, 0.205) #343434
 
 WriteConfig.WriteCFG('Minecraft', 'Application', 'v1.16.5', 'Comment', 'Games', 'mine.png', 'minecraft.x86', False)
 DotDesktop.Generate()
+os.remove('config.json')
 
 
 class dotDesktopUI(GridLayout):
@@ -26,6 +28,6 @@ class DotDesktopCreateApp(App):
     def build(self):
         return dotDesktopUI()
 
-Builder.load_file('ui.kv')
+Builder.load_file('UI-Markup.kv')
 dApp = DotDesktopCreateApp()
 dApp.run()
