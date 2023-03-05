2747c500a18040f2f779d5222b0b879c905fe62f
daniliammo <67186086+daniliammo@users.noreply.github.com>
2023-03-05 11:24:42 +0500

    Исправление кода

--- GenerateDotDesktop.py
+++ GenerateDotDesktop.py
@@ -1,41 +1,7 @@
-import json
-
-
 class DotDesktop:
 
-    def Generate():
-        with open('config.json') as Config:
-            data = json.load(Config)
-
-            for s in data['CFG']:
-                print('\nConfig:\n-------------------------------')
-
-                AppName = (s['Name'])           # str
-                print('Application Name:', AppName)
-
-                AppType = (s['Type'])           # str
-                print('Application Type:', AppType)
-
-                AppVersion = (s['Version'])     # str
-                print('Application Version:', AppVersion)
-
-                AppComment = (s['Comment'])     # str
-                print('Application Comment:', AppComment)
-
-                AppCategories = (s['Categories']) # str
-                print('Application Categories:', AppCategories)
-
-                AppIcon = (s['Icon'])           # str
-                print('Application Icon:', AppIcon)
-
-                AppExec = (s['Exec'])           # str
-                print('Application Execute:', AppExec)
-
-                AppTerminal = (s['Terminal'])   # bool
-                print('Application Terminal:', AppTerminal)
-
-                print('-------------------------------')
+    def Generate(AppName, Type, Version, Comment, Categories, Icon, Exec, Terminal):
 
         dotDesktop = open(AppName + '.desktop', 'w')
-        dotDesktop.write(f'[Desktop Entry]\nName={AppName}\nType={AppType}\nVersion={AppVersion}\nComment={AppComment}\nCategories={AppCategories}\nIcon={AppIcon}\nExec={AppExec}\nTerminal={AppTerminal}')
+        dotDesktop.write(f'[Desktop Entry]\nName={AppName}\nType={Type}\nVersion={Version}\nComment={Comment}\nCategories={Categories}\nIcon={Icon}\nExec={Exec}\nTerminal={Terminal}')
         dotDesktop.close()
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from GenerateDotDesktop import DotDesktop
-from ConfigWrite import WriteConfig
 from kivy.lang.builder import Builder
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
@@ -10,13 +9,8 @@ Config.set('graphics', 'width', '950')
 Config.set('graphics', 'height', '950')
 Window.clearcolor = (0.205, 0.205, 0.205) #343434
 
-WriteConfig.WriteCFG('Minecraft', 'Application', 'v1.16.5', 'Comment', 'Games', 'mine.png', 'minecraft.x86', False)
-DotDesktop.Generate()
-os.remove('config.json')
-
 
 class dotDesktopUI(GridLayout):
-    from ConfigWrite import WriteConfig
     from GenerateDotDesktop import DotDesktop
 
 
