42f985088dcc0742ff1db72c8e1e22909e2b7885
DANILIAMMO <67186086+daniliammo@users.noreply.github.com>
2023-06-06 20:38:33 +0500

    удалено ненужное

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.config import Config
 from kivy.core.window import Window
+
 Config.set('graphics', 'width', '950')
 Config.set('graphics', 'height', '950')
-Window.clearcolor = (0.205, 0.205, 0.205) #343434
+Window.clearcolor = (0.205, 0.205, 0.205)  # 343434
 
 
 class dotDesktopUI(GridLayout):
@@ -13,11 +14,11 @@ class dotDesktopUI(GridLayout):
 
 
 class DotDesktopCreateApp(App):
-
     title = "Мастер Создания .Desktop"
 
     def build(self):
         return dotDesktopUI()
+
 
 Builder.load_file('UI-Markup.kv')
 dApp = DotDesktopCreateApp()
